34c474aa9397004d88c91f938a51325812d23ecd
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2020-01-22 04:58:56 +0000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Binary Tree Implementation In Rust
+This repository is an implementation of Binary Tree without using  Box or other smart pointers. 
 ## Data Structure Definition 
--- src/main.rs
+++ src/main.rs
@@ -21,24 +21,40 @@ impl<'a> Tree<'a, i32> {
         match self.value {
             None => {
                 self.value = Some(value);
-                //break;
             }
             Some(val) => {
-                // stop case
+                // right insertion stop case
                 if val < value && self.right.is_none() {
                     self.right = Some(arena.alloc(Tree {
                         left: None,
                         value: Some(value),
                         right: None,
                     }));
-                    //break;
                 }
+                // moving right
                 if val < value && self.right.is_some() {
                     match self.right {
                         Some(ref mut tmp) => {
                             tmp.insert(arena, value);
                         }
-                        None => todo!(),
+                        None => println!("how come !?"),
+                    }
+                }
+                // left insertion stop case
+                if val > value && self.left.is_none() {
+                    self.left = Some(arena.alloc(Tree {
+                        left: None,
+                        value: Some(value),
+                        right: None,
+                    }));
+                }
+                // moving left
+                if val > value && self.left.is_some() {
+                    match self.left {
+                        Some(ref mut tmp) => {
+                            tmp.insert(arena, value);
+                        }
+                        None => println!("how come !?"),
                     }
                 }
             }
@@ -49,11 +65,8 @@ impl<'a> Tree<'a, i32> {
 fn main() {
     let arena = Arena::new();
     let mut b = Tree::init();
-    //Tree::insert(&mut b,&arena,1);
-    //Tree::insert(&mut b,&arena,2);
-    //Tree::insert(&mut b,&arena,3);
-    b.insert(&arena, 1);
-    b.insert(&arena, 2);
     b.insert(&arena, 3);
+    b.insert(&arena, 2);
+    b.insert(&arena, 1);
     println!("{:?}", b);
 }
