254a7e9a8f3e6182260be077654bf21daccfc5a0
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2020-01-31 02:32:20 +0000

    delete function for left hand side finished :D

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,32 @@ impl<'a> Tree<'a, i32> {
             }
         }
     }
+
+    fn del(&mut self, value: i32) {
+        if self.left.as_ref().unwrap().value.unwrap() >= value {
+	    //stop case for not founding
+            if self.left.is_none() {
+		println!("didn't find the given value on tree");
+		return;
+	    }
+	    //no child
+	    if self.left.as_ref().unwrap().value.unwrap() == value && self.left.as_ref().unwrap().left.is_none() && self.left.as_ref().unwrap().right.is_none(){
+		self.left = None;
+		return;
+	    }
+	    if self.left.as_ref().unwrap().value.unwrap() == value && self.left.as_ref().unwrap().left.is_some(){
+		self.left.as_mut().unwrap().value = Some(self.left.as_ref().unwrap().left.as_ref().unwrap().value.unwrap());
+		self.left.as_mut().unwrap().left = None;
+		return;
+	    }
+	    if self.left.as_ref().unwrap().value.unwrap() == value && self.left.as_ref().unwrap().right.is_some(){
+                self.left.as_mut().unwrap().value = Some(self.left.as_ref().unwrap().right.as_ref().unwrap().value.unwrap());
+                self.left.as_mut().unwrap().right = None;
+                return;                                                                                                                
+            } 
+	    self.left.as_mut().unwrap().del(value);
+        }
+    }
 }
 
 fn main() {
@@ -68,5 +94,6 @@ fn main() {
     b.insert(&arena, 3);
     b.insert(&arena, 2);
     b.insert(&arena, 1);
+    b.del(1);
     println!("{:?}", b);
 }
