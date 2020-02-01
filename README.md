eedba99d0564d9fcb2eef8b0df5198a507adb747
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2020-02-01 20:12:49 +0000

    delete function finished

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Binary Tree Implementation In Rust
-This repository is an implementation of Binary Tree without using  Box or other smart pointers. 
+This repository is an implementation of Binary Tree without using any unsafe block or any Box or other smart pointers.
+I implement this just for better understanding of pointers and life times in rust, I hope it will be helpful.
 ## Data Structure Definition 
--- src/main.rs
+++ src/main.rs
@@ -61,29 +61,104 @@ impl<'a> Tree<'a, i32> {
         }
     }
 
+    fn findMin(&mut self)->Option<i32>{
+	//println!("<--------->");
+	let mut val:Option<i32> = None;
+	if (self.left.as_ref().unwrap().left.is_none()){
+	    val = Some(self.left.as_ref().unwrap().value.unwrap());
+	    //println!("{:?}",self.left.as_ref().unwrap().value);
+	    self.left = None;
+	    //println!("{:?}",val);
+	    //return val;
+	}
+	if val.is_none(){
+	    //println!("---------");
+	    return self.left.as_mut().unwrap().findMin();
+	}
+	//println!("slm {:?}",val);
+	return val;
+    }
+    
     fn del(&mut self, value: i32) {
-        if self.left.as_ref().unwrap().value.unwrap() >= value {
-	    //stop case for not founding
+	if self.value.unwrap() == value
+	    && self.left.is_some()
+	    && self.right.is_some(){
+		self.value = self.right.as_mut().unwrap().findMin();
+		return;
+	    }
+        if self.value.unwrap() >= value {
+            //stop case for not founding
             if self.left.is_none() {
-		println!("didn't find the given value on tree");
-		return;
-	    }
-	    //no child
-	    if self.left.as_ref().unwrap().value.unwrap() == value && self.left.as_ref().unwrap().left.is_none() && self.left.as_ref().unwrap().right.is_none(){
-		self.left = None;
-		return;
-	    }
-	    if self.left.as_ref().unwrap().value.unwrap() == value && self.left.as_ref().unwrap().left.is_some(){
-		self.left.as_mut().unwrap().value = Some(self.left.as_ref().unwrap().left.as_ref().unwrap().value.unwrap());
-		self.left.as_mut().unwrap().left = None;
-		return;
-	    }
-	    if self.left.as_ref().unwrap().value.unwrap() == value && self.left.as_ref().unwrap().right.is_some(){
-                self.left.as_mut().unwrap().value = Some(self.left.as_ref().unwrap().right.as_ref().unwrap().value.unwrap());
+                println!("didn't find the given value on tree");
+                return;
+            }
+            //no child
+            if self.left.as_ref().unwrap().value.unwrap() == value
+                && self.left.as_ref().unwrap().left.is_none()
+                && self.left.as_ref().unwrap().right.is_none()
+            {
+                self.left = None;
+                return;
+            }
+            if self.left.as_ref().unwrap().value.unwrap() == value
+                && self.left.as_ref().unwrap().left.is_some()
+            {
+                self.left.as_mut().unwrap().value = Some(
+                    self.left
+                        .as_ref()
+                        .unwrap()
+                        .left
+                        .as_ref()
+                        .unwrap()
+                        .value
+                        .unwrap(),
+                );
+                self.left.as_mut().unwrap().left = None;
+                return;
+            }
+            if self.left.as_ref().unwrap().value.unwrap() == value
+                && self.left.as_ref().unwrap().right.is_some()
+            {
+                self.left.as_mut().unwrap().value = Some(
+                    self.left
+                        .as_ref()
+                        .unwrap()
+                        .right
+                        .as_ref()
+                        .unwrap()
+                        .value
+                        .unwrap(),
+                );
                 self.left.as_mut().unwrap().right = None;
-                return;                                                                                                                
-            } 
-	    self.left.as_mut().unwrap().del(value);
+                return;
+            }
+            self.left.as_mut().unwrap().del(value);
+        }
+        if (self.value.unwrap() <= value) {
+            if (self.right.is_none()) {
+                println!("didn't find the value");
+                return;
+            }
+            if (self.right.as_ref().unwrap().value.unwrap() == value
+                && self.right.as_ref().unwrap().left.is_none()
+                && self.right.as_ref().unwrap().right.is_none())
+            {
+                self.right = None;
+                return;
+            }
+	    if(self.right.as_ref().unwrap().value.unwrap() == value
+	       && self.right.as_ref().unwrap().right.is_some()){
+		self.right.as_mut().unwrap().value = self.right.as_ref().unwrap().right.as_ref().unwrap().value;
+		self.right.as_mut().unwrap().right = None;
+		return;
+	    }
+	    if(self.right.as_ref().unwrap().value.unwrap() == value
+               && self.right.as_ref().unwrap().left.is_some()){                                                                   
+                self.right.as_mut().unwrap().value = self.right.as_ref().unwrap().left.as_ref().unwrap().value;
+                self.right.as_mut().unwrap().left = None;
+                return;
+            }
+	    self.right.as_mut().unwrap().del(value);
         }
     }
 }
@@ -91,9 +166,17 @@ impl<'a> Tree<'a, i32> {
 fn main() {
     let arena = Arena::new();
     let mut b = Tree::init();
-    b.insert(&arena, 3);
-    b.insert(&arena, 2);
-    b.insert(&arena, 1);
-    b.del(1);
+    b.insert(&arena, 50);
+    b.insert(&arena, 30);
+    b.insert(&arena, 70);
+    b.insert(&arena, 20);
+    b.insert(&arena, 40);
+    b.insert(&arena, 60);
+    b.insert(&arena,80);
+    //let tmp = b.findMin();
+    println!("{:?}\n", b);
+    b.del(20);
+    b.del(30);
+    b.del(50);
     println!("{:?}", b);
 }
